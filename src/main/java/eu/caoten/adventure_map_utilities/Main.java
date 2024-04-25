cff4c89e1434361786e906b013af034ce0a842ca
Caoten_hoch2 <jerik.donnermeyer@gmail.com>
2024-04-25 17:47:22 +0200

    Update for 1.20.5

--- src/main/java/eu/caoten/adventure_map_utilities/Main.java
+++ src/main/java/eu/caoten/adventure_map_utilities/Main.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 
 public class Main implements ModInitializer {
 	public static final String MOD_ID = "adventure_map_utilities";
-	public static final String MOD_VERSION = "1.0.5";
+	public static final String MOD_VERSION = "1.0.7";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static boolean ENABLED_KEYBINDINGS = true;
 
